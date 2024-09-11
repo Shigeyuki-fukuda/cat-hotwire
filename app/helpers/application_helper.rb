ea34b25169acdcc0f1ac1dfb54a1dbf4535374fb
Shigeyuki-fukuda <27620649+Shigeyuki-fukuda@users.noreply.github.com>
2024-09-11 23:19:14 +0900

    feat: サイドナビゲーションのリンク先と現在アクティブなタブとアイコンをスタイリングするためのヘルパーメソッドを追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def sidebar_link_to(path, emoji, text)
+    classes = %w[my-1 nav-link text-white]
+    classes << "active" if current_page?(path)
+
+    link_to(path, class: classes) do
+      # tag.spanは RailsのHTMLタグを生成するヘルパーメソッド
+      # https://qiita.com/jkr_2255/items/a2d153c0c8198f492084
+      tag.span(class: "me-2") { emoji } + tag.span { text }
+    end
+  end
 end
