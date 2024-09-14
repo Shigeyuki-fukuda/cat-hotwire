339c3c1dadcb63174b662118627f5e741995aca1
Shigeyuki-fukuda <27620649+Shigeyuki-fukuda@users.noreply.github.com>
2024-09-14 09:06:11 +0900

    feat: Catモデルのバリデーション追加

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -1,4 +1,7 @@
 class Cat < ApplicationRecord
+  validates :name, presence: true
+  validates :age, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   # 検索可能な属性を指定
   # refs: https://qiita.com/Yamashita-Taiki/items/764be62a8c20485ece54
   def self.ransackable_attributes(auth_object = nil)
